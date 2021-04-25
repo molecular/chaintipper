62f031eb719c2f2f21782315ff94f38933881818
molecular <molec@posteo.de>
2021-04-25 08:18:40 +0200

    rework qt architecture: new class WalletUI encapsulating all wallet-specific qt stuff

--- qt.py
+++ qt.py
@@ -8,9 +8,11 @@ import weakref
 
 from electroncash.i18n import _
 from electroncash.plugins import BasePlugin, hook
+from electroncash_gui.qt import ElectrumWindow
 from electroncash_gui.qt.util import destroyed_print_error
 from electroncash.util import finalization_print_error
 from electroncash_gui.qt.main_window import StatusBarButton
+from electroncash.util import PrintError
 from electroncash.wallet import Abstract_Wallet
 
 from . import ui
@@ -56,13 +58,12 @@ class ChaintipperButton(StatusBarButton):
 		else:
 			raise TypeError(f"Expected a wallet or a window instance, instead got {type(wallet_or_window)}")
 
-	def __init__(self, plugin, wallet):
+	def __init__(self, wallet_ui):
 		super().__init__(icon_chaintip, fullname, self.toggle_active)
 
 		self.is_active = False
 
-		self.plugin = plugin
-		self.wallet = wallet
+		self.wallet_ui = wallet_ui
 
 		self.action_toggle = QAction(_("Active on this wallet"))
 		self.action_toggle.setCheckable(True)
@@ -73,7 +74,7 @@ class ChaintipperButton(StatusBarButton):
 		action_wsettings = QAction(_("Wallet-specific Settings..."), self)
 		action_wsettings.triggered.connect(self.show_wallet_settings)
 		action_settings = QAction(_("Global Settings..."), self)
-		action_settings.triggered.connect(self.plugin.show_settings_dialog)
+		action_settings.triggered.connect(self.wallet_ui.show_settings_dialog)
 		action_separator2 = QAction(self); action_separator2.setSeparator(True)
 
 		self.addActions([self.action_toggle, action_separator1,
@@ -89,12 +90,12 @@ class ChaintipperButton(StatusBarButton):
 			self.setIcon(icon_chaintip)
 			self.setToolTip(_('ChainTipper is active on this wallet'))
 			self.setStatusTip(_('Chaintiper - Active'))
-			self.plugin.activate(self.wallet)
+			self.wallet_ui.activate()
 		else:
 			self.setIcon(icon_chaintip_gray)
 			self.setToolTip(_('ChainTipper not active on this wallet'))
 			self.setStatusTip(_('Chaintiper - Inactive (click to activate on this wallet)'))
-			self.plugin.deactivate(self.wallet)
+			self.wallet_ui.deactivate()
 
 	def toggle_active(self):
 		if self.is_active:
@@ -104,7 +105,7 @@ class ChaintipperButton(StatusBarButton):
 		self.update_state()
 
 	def show_wallet_settings(self):
-		win = getattr(self.wallet, '_chaintipper_settings_window', None)
+		win = getattr(self.wallet_ui.wallet, '_chaintipper_settings_window', None)
 		if not win:
 			win = WalletSettingsDialog(
 				ChaintipperButton.get_suitable_dialog_window_parent(self.wallet),
@@ -114,6 +115,91 @@ class ChaintipperButton(StatusBarButton):
 		win.show()
 		win.raise_()
 
+class WalletUI(PrintError):
+	"""
+	Encapsulates UI for a given wallet and associated window.
+	Plugin class will instantiate one WalletUI per wallet.
+	"""
+	def __init__(self, wallet: Abstract_Wallet, window: ElectrumWindow):
+		self.window = window
+		self.wallet = wallet
+		self.wallet_name = self.wallet.basename()
+
+		self.widgets = weakref.WeakSet() # widgets we made, that need to be hidden & deleted when plugin is disabled
+		self.wallet_tab = None
+		self.tab = None
+		self.l = None
+		self.previous_tab_index = None
+
+		self.setup_button()
+
+	def setup_button(self):
+		# setup chaintipper button
+		sbmsg = _('ChainTipper disabled, click to enable')
+		sbbtn = ChaintipperButton(self)
+
+		# bit of a dirty hack, to insert our status bar icon (always using index 4, should put us just after the password-changer icon)
+		sb = self.window.statusBar()
+		sb.insertPermanentWidget(4, sbbtn)
+		self.widgets.add(sbbtn)
+		self.window._chaintipper_button = weakref.ref(sbbtn)
+
+	def activate(self):
+		"""
+		Will be called by the ChaintipperButton on activation
+		"""
+		self.add_ui()
+		self.refresh_ui()
+		self.show_chaintipper_tab()
+
+	def deactivate(self):
+		"""
+		Will be called by the ChaintipperButton on deactivation
+		"""
+		self.remove_ui()
+		self.show_previous_tab()
+		# self.close_wallet(wallet) # TODO: this might be misuse
+
+	def show_chaintipper_tab(self):
+		if self.tab:
+			self.previous_tab_index = self.window.tabs.currentIndex()
+			self.window.tabs.setCurrentIndex(self.window.tabs.indexOf(self.tab))
+
+	def show_previous_tab(self):
+		self.print_error("previous tab index:", self.previous_tab_index)
+		if self.previous_tab_index != None:
+			self.window.tabs.setCurrentIndex(self.previous_tab_index)
+			self.previous_tab_index = None
+
+	def add_ui(self):
+		self.l = ui.LoadRWallet(self.window, self, self.wallet_name)
+		self.tab = self.window.create_list_tab(self.l)
+
+		self.window.tabs.addTab(self.tab, icon_chaintip, _('ChainTipper'))
+
+	def remove_ui(self):
+		if self.l:
+			for widget in self.widgets:
+				if widget and callable(getattr(widget, 'kill_join', None)):
+					widget.kill_join()  # kill thread, wait for up to 2.5 seconds for it to exit
+				if widget and callable(getattr(widget, 'clean_up', None)):
+					widget.clean_up()  # clean up wallet and stop its threads
+			if self.tab:
+				self.window.tabs.removeTab(self.window.tabs.indexOf(self.tab))
+				self.tab.deleteLater()
+				self.print_error("Removed UI for", self.wallet_name)
+			self.tab = None
+			self.l = None
+
+	def refresh_ui(self):
+		self.tab: self.tab.update()
+		if self.l: self.l.update()
+
+	def show_settings_dialog(self):
+		self.print_error("show_settings_dialog() not implemented")
+
+	def close(self):
+		self.print_error("walletui.close() called")
 
 class Plugin(BasePlugin):
 	electrumcash_qt_gui = None
@@ -121,13 +207,7 @@ class Plugin(BasePlugin):
 	def __init__(self, parent, config, name):
 		BasePlugin.__init__(self, parent, config, name)
 
-		self.widgets = weakref.WeakSet() # widgets we made, that need to be hidden & deleted when plugin is disabled
-
-		# all this stuff is stored by wallet_name (right?), why not use a class for this and migrate the functions also... much cleaner.
-		self.wallet_windows = {}
-		self.lw_tabs = {}
-		self.lw_tab = {}
-		self.previous_tab_index = {}
+		self.wallet_uis = {}
 
 	def fullname(self):
 		return 'ChainTipper'
@@ -142,8 +222,10 @@ class Plugin(BasePlugin):
 		"""
 		BasePlugin callback called when the wallet is disabled among other things.
 		"""
-		for window in list(self.wallet_windows.values()):
-			self.close_wallet(window.wallet)
+		# for window in list(self.wallet_windows.values()):
+		# 	self.close_wallet(window.wallet)
+		for wallet_ui in list(self.wallet_uis.values()):
+			wallet_ui.close()
 
 	@hook
 	def update_contact(self, address, new_entry, old_entry):
@@ -160,111 +242,38 @@ class Plugin(BasePlugin):
 		"""
 		self.electrumcash_qt_gui = qt_gui
 		# We get this multiple times.  Only handle it once, if unhandled.
-		if len(self.wallet_windows):
+		if len(self.wallet_uis):
 			return
 
 		# These are per-wallet windows.
 		for window in self.electrumcash_qt_gui.windows:
 			self.load_wallet(window.wallet, window)
 
-	def activate(self, wallet):
-		"""
-		Will be called by the ChaintipperButton on activation
-		"""
-		wallet_name = wallet.basename()
-		window = self.wallet_windows[wallet_name]
-		self.add_ui_for_wallet(wallet_name, window)
-		self.refresh_ui_for_wallet(wallet_name)
-		self.show_chaintipper_tab(wallet_name)
-
-	def deactivate(self, wallet):
-		"""
-		Will be called by the ChaintipperButton on deactivation
-		"""
-		wallet_name = wallet.basename()
-		self.show_previous_tab(wallet_name)
-		window = self.wallet_windows[wallet_name]
-		self.remove_ui_for_wallet(wallet_name, window)
-		# self.close_wallet(wallet) # TODO: this might be misuse
-
 	@hook
 	def load_wallet(self, wallet, window):
 		"""
 		Hook called when a wallet is loaded and a window opened for it.
+		Instantiates WalletUI for given wallet in given window and stores it in wallet_uis 
 		"""
 		wallet_name = window.wallet.basename()
+
 		self.print_error("load_wallet(", wallet_name,")")
-		self.wallet_windows[wallet_name] = window
-		self.print_error("wallet_windows:", self.wallet_windows)
-		self.print_error("wallet loaded")
+
+		wallet_ui = WalletUI(wallet, window)
+		self.wallet_uis[wallet_name] = wallet_ui
+		#self.wallet_windows[wallet_name] = window
 
 		# self.add_ui_for_wallet(wallet_name, window)
 		# self.refresh_ui_for_wallet(wallet_name)
 
-		# setup chaintipper button
-		active = False
-		if not active:
-			sbmsg = _('ChainTipper disabled, click to enable')
-		sbbtn = ChaintipperButton(self, wallet)
-
-		# bit of a dirty hack, to insert our status bar icon (always using index 4, should put us just after the password-changer icon)
-		sb = window.statusBar()
-		sb.insertPermanentWidget(4, sbbtn)
-		self.widgets.add(sbbtn)
-		window._chaintipper_button = weakref.ref(sbbtn)
-
 	@hook
 	def close_wallet(self, wallet):
-		self.print_error("************************ close_wallet")
-		wallet_name = wallet.basename()
-		window = self.wallet_windows[wallet_name]
-		del self.wallet_windows[wallet_name]
-		self.remove_ui_for_wallet(wallet_name, window)
+		self.print_error("************************ close_wallet (currently does nothing)")
+		#self.wallet_uis[wallet.basename()].close()
+		# window = self.wallet_windows[wallet_name]
+		# del self.wallet_windows[wallet_name]
+		# self.remove_ui_for_wallet(wallet_name, window)
 
-	def add_ui_for_wallet(self, wallet_name, window):
-		l = ui.LoadRWallet(window, self, wallet_name)
-		tab = window.create_list_tab(l)
-		self.lw_tabs[wallet_name] = tab
-		self.lw_tab[wallet_name] = l
-
-		window.tabs.addTab(tab, icon_chaintip, _('ChainTipper'))
-
-	def remove_ui_for_wallet(self, wallet_name, window):
-		wallet_tab = self.lw_tabs.get(wallet_name)
-		widget = self.lw_tab.get(wallet_name)
-		if wallet_tab is not None:
-			if widget and callable(getattr(widget, 'kill_join', None)):
-				widget.kill_join()  # kill thread, wait for up to 2.5 seconds for it to exit
-			if widget and callable(getattr(widget, 'clean_up', None)):
-				widget.clean_up()  # clean up wallet and stop its threads
-			del self.lw_tab[wallet_name]
-			del self.lw_tabs[wallet_name]
-			if wallet_tab:
-				i = window.tabs.indexOf(wallet_tab)
-				window.tabs.removeTab(i)
-				wallet_tab.deleteLater()
-				self.print_error("Removed UI for", wallet_name)
-
-	def refresh_ui_for_wallet(self, wallet_name):
-		wallet_tab = self.lw_tabs.get(wallet_name)
-		if wallet_tab:
-			wallet_tab.update()
-		wallet_tab = self.lw_tab.get(wallet_name)
-		if wallet_tab:
-			wallet_tab.update()
-
-	def show_chaintipper_tab(self, wallet_name):
-		window = self.wallet_windows[wallet_name]
-		wallet_tab = self.lw_tabs.get(wallet_name)
-		if wallet_tab:
-			self.previous_tab_index[wallet_name] = window.tabs.currentIndex()
-			window.tabs.setCurrentIndex(window.tabs.indexOf(wallet_tab))
-
-	def show_previous_tab(self, wallet_name):
-		window = self.wallet_windows[wallet_name]
-		if wallet_name in self.previous_tab_index:
-			window.tabs.setCurrentIndex(self.previous_tab_index[wallet_name])
-			del self.previous_tab_index[wallet_name]
 
 	# def switch_to(self, mode, wallet_name, recipient_wallet, time, password):
 	# 	window=self.wallet_windows[wallet_name]
@@ -289,5 +298,3 @@ class Plugin(BasePlugin):
 	# 		self.print_error(repr(e))
 	# 		return
 
-	def show_settings_dialog(self):
-		self.print_error("show_settings_dialog() not implemented")
