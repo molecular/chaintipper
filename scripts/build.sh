6b8e2ae2c63c05dc30df8455f4e1c314accf6f39
molecular <molec@posteo.de>
2021-05-06 08:34:18 +0200

    remove qresource.py, fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)/..
 
 pyrcc5 qresources.qrc > qresources.py
 
-bundled_deps/prepare_bundled_deps.sh
+bundled_deps/download_check_unpack_bundled_deps.sh
+bundled_deps/applay_patch.sh # results in patched deps in bundled_deps/patched/
 
 popd
