10eb724bf6d6136eea6809fde057734e1187fc1a
molecular <molec@posteo.de>
2021-05-08 08:01:09 +0200

    fix typo in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)/..
 pyrcc5 qresources.qrc > qresources.py
 
 bundled_deps/download_check_unpack_bundled_deps.sh
-bundled_deps/applay_patch.sh # results in patched deps in bundled_deps/patched/
+bundled_deps/apply_patch.sh # results in patched deps in bundled_deps/patched/
 
 popd
