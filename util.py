a56cb8f74f3c0eaf4700acb79cf7c932b979568c
molecular <molec@posteo.de>
2021-05-10 14:07:40 +0200

    new flag to read_config, write_config "commit":bool

--- util.py
+++ util.py
@@ -5,17 +5,19 @@ def has_config(wallet, key: str):
 		return False
 	return len(wallet.storage.get(key)) > 0
 
-def read_config(wallet, key: str, default=None):
+def read_config(wallet, key: str, default=None, commit=True):
 	"""convenience function to write to wallet storage prefixing key with 'chaintipper_'"""
 	key = "chaintipper_" + key
 	v = wallet.storage.get(key)
 	if v is None:
 		v = default
-		write_config(wallet, key, v)
+		write_config(wallet, key, v, commit)
 	return v
 
-def write_config(wallet, key: str, value):
+def write_config(wallet, key: str, value, commit=True):
 	"""convenience to read from wallet storage prefixing key with 'chaintipper_'"""
-	key = "chaintipper_" + key
+	if key[:12] != "chaintipper_":
+		key = "chaintipper_" + key
 	wallet.storage.put(key, value)
-	wallet.storage._write() # commit to hd
+	if commit:
+		wallet.storage._write() # commit to hd
