89d182ca768aefbda8d360c5ff298a9bf24361f5
molecular <molec@posteo.de>
2021-05-10 14:06:57 +0200

    change default autopay limit

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ c = {
 		
 		"default_autopay": False,
 		"default_autopay_use_limit": False,
-		"default_autopay_limit_bch": "0.01",
+		"default_autopay_limit_bch": "0.0001",
 		"default_autopay_disallow_default": False,
 		"default_autopay_disallow_linked": False,
 }
