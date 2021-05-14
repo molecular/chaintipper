fbe5ad18df5a505a48ebc705c1543ea232d80e1a
molecular <molec@posteo.de>
2021-05-14 18:24:48 +0200

    remove latest_version from git

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,7 +28,29 @@ cd ..
 
 sha256sum ChainTipper*.zip > "SHA256.ChainTipper.txt"
 
+# create lastest_release.json
+sha256=$(sha256sum ${zipfile} | cut -f 1 -d " ")
+uri='http://criptolayer.net/Pk4p2VyxVtOAkWzq/'${zipfile}
+
+echo -ne "please sign:\n\n$version,$uri,$sha256\n\n"
+read sig
+
+echo -ne '{
+	"version": "'${version}'",
+	"uri": "'${uri}'",
+	"sha256": "'${sha256}'",
+	"sig_ca": "molecular#123",
+	"sig_addr": "bitcoincash:qzz3zl6sl7zahh00dnzw0vrs0f3rxral9uedywqlfw",
+	"sig": "'${sig}'"
+}
+' > update_checker/latest_version.json
+
 # run any post-release copying 
 if [ -e "scripts/local_release.sh" ]; then
 	scripts/local_release.sh ${zipfile}
+fi
+
+# deploy to distribution location
+if [ -e "scripts/deploy.sh" ]; then
+	echo not running scripts/deploy.sh ${zipfile}
 fi
